d13b0c8e6863b78ff5df246663da4a1d2f72adf1
Amit Deshmukh <shivpatil308@gmail.com>
2023-10-28 18:56:03 +0530

    day5 task added

--- youtube/src/components/VideoContainer.jsx
+++ youtube/src/components/VideoContainer.jsx
@@ -2,26 +2,67 @@ import React, { useEffect, useState } from 'react';
 import {YOUTUBE_VIDEOS_API} from "../components/utils/contentApi"
 import VideoCard from './VideoCard';
 import { Link } from 'react-router-dom';
+import {  useSelector } from 'react-redux';
+
 
 const VideoContainer = () => {
   const [videos,setVideos]=useState([]);
-  // console.log(videos)
+  const [fdeta,setFdeta]=useState([]);
+  // console.log(fdeta)
+  
+
+  // ------------------------------------------------------------------------------
+  // SEARCH FUNCTIONALLATY
+      const searchItems=useSelector((store)=>store.searchInp.searchText);
+      // console.log(searchItems[0])
+
+
+      const DetaFun=(elemet,videos)=>{
+        //  console.log("element is "+videos);
+        const data=videos?.filter((ele)=>{
+          return ele?.snippet?.title?.toLowerCase().includes(elemet?.toLowerCase());
+        })
+
+        setFdeta("")
+        setFdeta(data)
+        // console.log(data)
+      }
+
+      
+        
+     
+      
+  // ------------------------------------------------------------------------------
 
 useEffect(()=>{
    getVideos()
 },[]);
 
+useEffect(() => {
+  DetaFun(searchItems[0],videos);
+  MyFunc(searchItems[0])
+}, [searchItems[0]])
+
+const MyFunc=(ele)=>{
+  // console.log("hmmm")
+     if(ele==="null"){
+      setFdeta(videos)
+     }
+}
+
 const getVideos= async ()=>{
     const data=await fetch(YOUTUBE_VIDEOS_API);
     const json=await data.json();
-    // console.log(json);
+    // console.log(json.items);
     setVideos(json.items)
+    setFdeta(json.items)
+    
 }
 
   return (
     <div className='flex flex-wrap'>
       {
-        videos?.map((video)=>
+        fdeta?.map((video)=>
    <Link to={"/watch?v="+ video.id} key={video?.id}><VideoCard info={video}/></Link>
 )
       }
